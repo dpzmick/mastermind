5ea3bb646fcbcb1f6a6f6bbb135241c40785e8ef
David Zmick <dpzmick@gmail.com>
2013-07-28 21:13:57 -0500

    first working game

---
+++ mastermind.py
@@ -0,0 +1,128 @@
+import unittest
+from random import randint
+from copy import deepcopy, copy
+import profile
+
+class TestMastermindGame(unittest.TestCase):
+    def test_constructor_pos_col(self):
+        master = Mastermind(positions=4, colors=6)
+        self.assertEqual(master.positions, 4)
+        self.assertEqual(master.colors, 6)
+    
+    def test_constructor_code_rand(self):
+        master = Mastermind(positions=4, colors=6)
+        self.fail("TODO")
+
+    def test_constructor_code_given(self):
+        code = [1,2,3,4]
+        master = Mastermind(code=code)
+        self.fail("TODO")
+
+    def test_check_guess_correct_all(self):
+        code = [1,2,3,4]
+        guess = [1,2,3,4]
+
+        master = Mastermind(positions=4, colors=6, code=code)
+        self.assertEqual(master.check_guess(guess), (4,0))
+
+    def test_check_guess_wrong_all(self):
+        code = [1,1,1,1]
+        guess = [2,2,2,2]
+
+        master = Mastermind(positions=4, colors=6, code=code)
+        self.assertEqual(master.check_guess(guess), (0,0))
+
+    def test_check_guess_a(self):
+        code = [1,2,3,4]
+        guess = [4,3,2,1]
+
+        master = Mastermind(positions=4, colors=6, code=code)
+        self.assertEqual(master.check_guess(guess), (0,4))
+
+    def test_check_guess_b(self):
+        code = [1,1,2,2]
+        guess = [2,2,5,5]
+
+        master = Mastermind(positions=4, colors=6, code=code)
+        self.assertEqual(master.check_guess(guess), (0,2))
+
+    def test_check_guess_c(self):
+        code = [1,4,4,3]
+        guess = [4,4,3,2]
+
+        master = Mastermind(positions=4, colors=6, code=code)
+        self.assertEqual(master.check_guess(guess), (1,2))
+
+    # TODO discover if there are more test cases
+        
+class Mastermind:
+    def __init__(self, positions=4, colors=6, code=None):
+        self.positions = positions
+        self.colors = colors
+
+        if code is None:
+            code = self.random_code()
+        else:
+            if not self.is_code_valid(code):
+                raise Exception("Not a valid code!")
+
+        self.code = {}
+        for color in range(1, self.colors + 1):
+            self.code[color] = [p for p, c in enumerate(code) if c == color]
+
+    def random_code(self):
+        code = []
+        for i in range(0, self.positions):
+            code.append(randint(1,self.colors))
+        return code
+    
+    def check_guess(self, guess):
+        code_copy = deepcopy(self.code)
+        correct = 0
+        wrong_location = 0
+        for p, c in enumerate(copy(guess)):
+            if p in code_copy[c]:
+                correct += 1
+                code_copy[c].remove(p)
+                guess.remove(c)
+
+        for p, c in enumerate(guess):
+            if len(code_copy[c]) > 0:
+                wrong_location += 1
+                code_copy[c].pop()
+
+        return (correct, wrong_location)
+
+    # TODO
+    def is_code_valid(self, code):
+        valid_colors = True
+        for c in code:
+            if c > self.colors:
+                valid_colors = False
+                break
+        valid_colors = valid_colors and len(set(code)) <= self.colors
+        return len(code) == self.positions and valid_colors
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+def game():
+    mastermind = Mastermind()
+    output = (0,0)
+    turn = 0
+    while output != (4,0):
+        turn += 1
+        print "[%d] Please enter a guess." % turn
+        guess = []
+        for i in range(0,mastermind.positions):
+            print "guess[%d]: " % i,
+            guess.append(int(raw_input()))
+
+        print "Your guess was: " + str(guess)
+
+        output = mastermind.check_guess(guess)
+
+        print "%d are correct" % output[0]
+        print "%d are the right color, but in the wrong place" % output[1]
+    print "Yay! You got it in %d turns" % turn
