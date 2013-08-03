8c8758d41e0290b0630d0235b574d9783caff61f
David Zmick <dpzmick@gmail.com>
2013-08-02 19:14:32 -0500

    forgot to stage a file

--- random_player.py
+++ random_player.py
@@ -1,32 +1,28 @@
-from mastermind import Mastermind
+from game import Game
 from random import randint
 
-turn_limit = 25
 games = 10000
-positions = 4
-colors = 6
 
+class RandomPlayer(Game):
+    def __init__(self):
+        Game.__init__(self, name="random_player")
 
-def game():
-    print "[random_player] creating new game"
-    print "[random_player] pos: %d colors: %d random code" % (positions, colors)
-    master = Mastermind(colors=colors, positions=positions)
-    for turn in range(1, turn_limit + 1):
-        guess = [randint(1, colors) for i in range(0, positions)]
-        res = master.check_guess(guess)
-        if res == (4,0):
-            return (True, turn)
-
-    return (False, turn_limit)
+    def game(self):
+        for turn in range(1, self.turn_limit + 1):
+            guess = [randint(1, self.colors) for i in range(0, self.positions)]
+            res = self.master.check_guess(guess)
+            if res == (4,0):
+                return (True, turn)
+        return (False, self.turn_limit)
 
 
 if __name__ == "__main__":
-    print "random_player starting"
     output = open('random_player.csv', 'w')
     output.write("game_number, turns, succeded\n")
     for i in range(1, games + 1):
+        player = RandomPlayer()
         print "[random_player] Game: %d of %d" % (i, games)
-        result = game()
+        result = player.game()
         print "[random_player] result: " + str(result)
         output.write("%d, %d, %s\n" % (i, result[1], result[0]))
 
