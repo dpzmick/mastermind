3cb53143d7604de7a9d1419b5ba65cd3895bee85
David Zmick <dpzmick@gmail.com>
2013-08-07 22:00:52 -0500

    refactored further, I think I'm happy with this

--- random_player.py
+++ random_player.py
@@ -1,12 +1,9 @@
-from game import Game
+from sim import Player, run_many_games
 from random import randint
 
 games = 10000
 
-class RandomPlayer(Game):
-    def __init__(self):
-        Game.__init__(self, name="random_player")
-
+class RandomPlayer(Player):
     def game(self):
         for turn in range(1, self.turn_limit + 1):
             guess = [randint(1, self.colors) for i in range(0, self.positions)]
@@ -15,15 +12,5 @@ class RandomPlayer(Game):
                 return (True, turn)
         return (False, self.turn_limit)
 
-
 if __name__ == "__main__":
-    output = open('random_player.csv', 'w')
-    output.write("game_number, turns, succeded\n")
-    for i in range(1, games + 1):
-        player = RandomPlayer()
-        print "[random_player] Game: %d of %d" % (i, games)
-        result = player.game()
-        print "[random_player] result: " + str(result)
-        output.write("%d, %d, %s\n" % (i, result[1], result[0]))
-
-    output.close()
+    run_many_games(RandomPlayer, 1, "random_player.csv")
---
+++ sim.py
@@ -0,0 +1,38 @@
+from mastermind import Mastermind
+
+class Player:
+    def __init__(self, turn_limit=16, positions=4, colors=6):
+    
+        self.log("starting")
+
+        self.turn_limit = turn_limit
+        self.positions = positions
+        self.colors = colors
+
+        self.log("creating new game, pos: %d, colors: %d, random code"
+                % (self.positions, self.colors))
+
+        self.master = Mastermind(colors=colors, positions=positions)
+
+    def log(self, message):
+        print "[%s]: %s" % (self.__class__.__name__, message)
+
+    # play the mastermind game 
+    def game(self):
+        return (False, -1)
+
+def run_many_games(class_name, trials, output_file,
+        turn_limit=16, positions=4, colors=6):
+
+    output = open(output_file, 'w')
+    output.write('game_number,turns,succeeded?\n')
+    for i in range(1, trials + 1):
+        print "[%s] game number: i" % class_name
+        result = class_name(turn_limit=turn_limit,
+                positions=positions, colors=colors).game()
+        
+        print "[%s] result: %d" % (class_name, result[1])
+        output.write("%d,%d,%s\n" % (i, result[1], result[0]))
+    
+    output.close()
+
