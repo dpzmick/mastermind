89f3817806c91c82e1b46db69d4bcaf9e562ac9e
David Zmick <dpzmick@gmail.com>
2015-02-05 21:32:57 -0600

    actually pushing

--- queued.py
+++ queued.py
@@ -1,23 +1,41 @@
 from redis import Redis
+import redis
 from rq import Queue
 from random_player import RandomPlayer
 from sim import run_many_games
+import time
 
-q = Queue('high', connection=Redis())
-jobs = []
+big = Queue('big', connection=Redis())
+small = Queue('small', connection=Redis())
+big_jobs = []
+small_jobs = []
 
-games_per_task = 2000
+games_per_big = 9500
+games_per_small = 6000
 
-games = 100000
-tasks = games / games_per_task
+big_hosts = 9
+small_hosts = 4
 
-print "Making %d tasks, %d games each, to run %d games" % (tasks,games_per_task,games)
+games = 5000000000
+#tasks = games / games_per_task
 
-for task in range(0, tasks):
-    jobs.append(q.enqueue(run_many_games, RandomPlayer, games_per_task,
-        redis_key="rp"))
+r = redis.StrictRedis("192.168.1.201", port=6379, db=0)
 
-while None in [job.result for job in jobs]:
-    continue
+while r.llen('rp') < games:
+    small_avail = small_hosts - len(filter(lambda x: x.result == None, small_jobs))
+    big_avail = big_hosts - len(filter(lambda x: x.result == None, big_jobs))
+
+    for i in range(0, big_avail):
+        big_jobs.append(big.enqueue(run_many_games, RandomPlayer, games_per_big,
+            redis_key="rp"))
+
+    for i in range(0, small_avail):
+        small_jobs.append(small.enqueue(run_many_games, RandomPlayer, games_per_small,
+            redis_key="rp"))
+    
+    #if small_avail > 0:
+    #    print "smalls: %d" % small_avail
+    #if big_avail > 0:
+    #    print "bigs: %d" % big_avail
 
 print "finished"
